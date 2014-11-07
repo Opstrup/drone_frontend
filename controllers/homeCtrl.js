159b7c6defbd71b5ff11068eed88645ba93fd1b1
Anders Opstrup <aopstrup@gmail.com>
2014-11-07 08:52:27 +0100

    Added marker for drone function

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -9,24 +9,27 @@ angular
     var droneLongitude = 10.191149711608887;
     var mapCenterLatitude = 56.172280736338074;
     var mapCenterLongitude = 10.191149711608887;
+    $scope.marker;
+
+    $scope.map = MapServices.initMap(mapCenterLatitude, mapCenterLongitude);
+    $scope.marker = MapServices.initDroneMarker();
 
     DroneServices.getAllDrones().then(function(allDronesInSystem){
       $scope.allDrones = allDronesInSystem;
     });
 
-    $scope.singleDroneInfo = function(nextEventID, droneID) {
+    $scope.updateView = function(nextEventID, droneID) {
+
+      DroneServices.getSingleDroneInfo(droneID).then(function(droneInformation){
+        $scope.droneInformation = droneInformation;
+        $scope.marker = MapServices.updateDroneMarker(droneInformation.latitude, droneInformation.longitude);
+        $scope.map = MapServices.updateMap(droneInformation.latitude, droneInformation.longitude);
+      });
+
       DroneServices.getWaypoints(nextEventID).then(function(waypointsForDrone){
         $scope.waypointsForDrone = waypointsForDrone;
       });
 
-      DroneServices.getSingleDroneInfo(droneID).then(function(droneInformation){
-        $scope.droneInformation = droneInformation;
-      });
-
-    };
-
-    $scope.map = MapServices.initMap(mapCenterLatitude, mapCenterLongitude);
-
-    $scope.marker = MapServices.makeDroneMarker(droneLatitude, droneLongitude);
+    }
 
   }]);
--- services/MapServices.js
+++ services/MapServices.js
@@ -22,20 +22,60 @@ angular
           },
 
           /**
-           * Mark where the given drone is on the map.
+           * Updates map center.
+           **/
+          updateMap: function(centerLatitude, centerLongitude)
+          {
+            return {
+                    center: {
+                      latitude: centerLatitude,
+                      longitude: centerLongitude
+                    },
+                    zoom: 17
+                  };
+          },
+
+          /**
+           * Initialize the mark for the drone.
            * Drone marke will always have id: 0.
            **/
-          makeDroneMarker: function(droneLatitude, droneLongitude)
+          initDroneMarker: function()
           {
             return {
                     id: 0,
-                    coords: {
-                      latitude: droneLatitude,
-                      longitude: droneLongitude
-                      }
                     }
           },
 
-          
+          /**
+           * Mark where the given drone is on the map.
+           * Drone marke will always have id: 0.
+           **/
+          updateDroneMarker: function(droneLatitude, droneLongitude)
+          {
+            return {
+                    coords: {
+                      latitude: droneLatitude,
+                      longitude: droneLongitude
+                    },
+                      icon: "https://dl.dropboxusercontent.com/u/4950226/indoor-hull-ardrone-large.jpg"
+                    }
+          },
+
+          /**
+           * Draws all the waypoints for the drone on the map.
+           * Input: waypoint array
+           **/
+          drawWaypointsMarker: function(waypoints)
+          {
+            return {
+                    coords: {
+                      latitude: droneLatitude,
+                      longitude: droneLongitude
+                    },
+                      icon: "https://dl.dropboxusercontent.com/u/4950226/indoor-hull-ardrone-large.jpg"
+                    }
+          },
+
+
         }
     }]);
