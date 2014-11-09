e0870cbe5bc89a14b742840e2502121768a816c5
Anders Opstrup <aopstrup@gmail.com>
2014-11-09 22:44:57 +0100

    Waypoints! Yaygit status

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -5,13 +5,11 @@ angular
   .controller('homeCtrl', ['$scope', 'DroneServices', 'MapServices', function($scope, DroneServices, MapServices) {
     $scope.title = "Home";
     $scope.droneStatus = "";
-    var droneLatitude = 56.172280736338074;
-    var droneLongitude = 10.191149711608887;
     var mapCenterLatitude = 56.172280736338074;
     var mapCenterLongitude = 10.191149711608887;
-    $scope.marker;
 
     $scope.map = MapServices.initMap(mapCenterLatitude, mapCenterLongitude);
+    $scope.options = {scrollwheel: false};
     $scope.marker = MapServices.initDroneMarker();
 
     DroneServices.getAllDrones().then(function(allDronesInSystem){
@@ -32,4 +30,30 @@ angular
 
     }
 
-  }]);
+    // $scope.waypointList = MapServices.drawWaypointsMarker();
+
+    /********* Trash code *************/
+     var createRandomMarker = function (i, idKey) {
+            if (idKey == null) {
+                idKey = "id";
+            }
+
+            var latitude = 56.171211605108816;
+            var longitude = 10.191653966903687;
+            var ret = {
+                latitude: latitude,
+                longitude: longitude,
+                title: 'm' + i
+            };
+            ret[idKey] = i;
+            return ret;
+        };
+        $scope.randomMarkers = [];
+
+        var markers = [];
+        for (var i = 0; i < 2; i++) {
+          markers.push(createRandomMarker(i))
+        }
+        $scope.waypointList = markers;
+
+}]);
--- services/MapServices.js
+++ services/MapServices.js
@@ -1,81 +1,102 @@
 'use strict'
 
 angular
-    .module('frontend.MapServices', [])
-    .factory('MapServices',['GoogleMapApi'.ns(), function(GoogleMapApi){
-        return{
+  .module('frontend.MapServices', [])
+  .factory('MapServices',['GoogleMapApi'.ns(), function(GoogleMapApi){
+    return{
 
-          /**
-           * Initialize the map on the page with the given center coords.
-           * Zoom level is 17.
-           * (The higher zoom level is the more zoom)
-           **/
-          initMap: function(centerLatitude, centerLongitude)
-          {
-            return {
-                    center: {
-                      latitude: centerLatitude,
-                      longitude: centerLongitude
-                    },
-                    zoom: 17
-                    };
-          },
+      /**
+       * Initialize the map on the page with the given center coords.
+       * Zoom level is 17.
+       * (The higher zoom level is the more zoom)
+       **/
+      initMap: function(centerLatitude, centerLongitude)
+      {
+        return {
+                center: {
+                  latitude: centerLatitude,
+                  longitude: centerLongitude
+                },
+                zoom: 17
+                };
+      },
 
-          /**
-           * Updates map center.
-           **/
-          updateMap: function(centerLatitude, centerLongitude)
-          {
-            return {
-                    center: {
-                      latitude: centerLatitude,
-                      longitude: centerLongitude
-                    },
-                    zoom: 17
-                  };
-          },
+      /**
+       * Updates map center.
+       **/
+      updateMap: function(centerLatitude, centerLongitude)
+      {
+        return {
+                center: {
+                  latitude: centerLatitude,
+                  longitude: centerLongitude
+                },
+                zoom: 17
+              };
+      },
 
-          /**
-           * Initialize the mark for the drone.
-           * Drone marke will always have id: 0.
-           **/
-          initDroneMarker: function()
-          {
-            return {
-                    id: 0,
-                    }
-          },
+      /**
+       * Initialize the mark for the drone.
+       * Drone marke will always have id: 0.
+       **/
+      initDroneMarker: function()
+      {
+        return {
+                  id: 0,
+                }
+      },
 
-          /**
-           * Mark where the given drone is on the map.
-           * Drone marke will always have id: 0.
-           **/
-          updateDroneMarker: function(droneLatitude, droneLongitude)
-          {
-            return {
-                    coords: {
-                      latitude: droneLatitude,
-                      longitude: droneLongitude
-                    },
-                      icon: "https://dl.dropboxusercontent.com/u/4950226/indoor-hull-ardrone-large.jpg"
-                    }
-          },
+      /**
+       * Mark where the given drone is on the map.
+       * Drone marke will always have id: 0.
+       **/
+      updateDroneMarker: function(droneLatitude, droneLongitude)
+      {
+        return {
+                  coords:
+                        {
+                          latitude: droneLatitude,
+                          longitude: droneLongitude
+                        },
+                  icon: "https://dl.dropboxusercontent.com/u/4950226/indoor-hull-ardrone-large.jpg"
+                }
+      },
 
-          /**
-           * Draws all the waypoints for the drone on the map.
-           * Input: waypoint array
-           **/
-          drawWaypointsMarker: function(waypoints)
-          {
-            return {
-                    coords: {
-                      latitude: droneLatitude,
-                      longitude: droneLongitude
-                    },
-                      icon: "https://dl.dropboxusercontent.com/u/4950226/indoor-hull-ardrone-large.jpg"
-                    }
-          },
+      /**
+       * Draws all the waypoints for the drone on the map.
+       * Input: waypoint array
+       **/
+      initWaypointsMarker: function()
+      {
+        return {
+                  id: 1,
+                }
+      },
+
+      /**
+       * Draws all the waypoints for the drone on the map.
+       * Input: waypoint array
+       **/
+      drawWaypointsMarker: function()
+      {
+        return  [
+                  {
+                    latitude: 56.171211605108816,
+                    longtitude: 10.191653966903687,
+                    title: "title1",
+                    id: 1
+                    // icon: "http://icons.iconarchive.com/icons/ncrow/mega-pack-1/16/Yahoo-Messenger-icon.png"
+                  },
+                  {
+                    latitude: 56.171360926901414,
+                    longtitude: 10.190484523773193,
+                    title: "title2",
+                    id: 2
+                    // icon: "http://icons.iconarchive.com/icons/ncrow/mega-pack-1/16/Yahoo-Messenger-icon.png"
+                  }
+                ]
+      },
 
 
-        }
-    }]);
+    }
+  }]);
